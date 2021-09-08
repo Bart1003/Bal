60f1f5b7e61728cde8375f67fe4fef7bd2752927
Bartvan6 <57255021+Bart1003@users.noreply.github.com>
2021-09-08 09:40:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var x, y;
+
+function setup() {
+  	createCanvas(500, 400);
+
+    x = 0;
+    y = 200;
+    v = 3;
+}
+
+function draw() {
+  background(225);
+  
+  ellipse(x,y,50,50);
+  x = x + v;
+
+  if(x <= 0 || x >= 500)
+    v = v * -1;
+  
+}
